13abde18a6c725fe81221334872c83f93d071b0c
Dwiky Kurniawan <dwiky24kurniawan@gmail.com>
2024-03-21 15:04:53 +0700

    mengubah element locator

--- cypress/pageobjects/loginPage.js
+++ cypress/pageobjects/loginPage.js
@@ -1,7 +1,7 @@
 export let LoginPage = {
-  getTextboxUsername: () => cy.get("#user-name").should("be.visible"),
+  getTextFieldUsername: () => cy.get("#user-name").should("be.visible"),
   
-  getTextboxPassbord: () => cy.get("#password").should("be.visible"),
+  getTextFieldPassword: () => cy.get("#password").should("be.visible"),
 
   getButtonLogin: () => cy.get("#login-button").should("be.visible"),
 
@@ -9,10 +9,10 @@ export let LoginPage = {
     cy.url().should("include", "/inventory.html"),
 
   AssertAppLogo: () =>
-    cy.get(".app_logo").should("have.text", "Swag Labs").should("be.visible"),
+    cy.get(".app_logo").should("have.text", "Swag Labs").and("be.visible"),
 
   AssertProductsText: () =>
-    cy.get(".title").should("have.text", "Products").should("be.visible"),
+    cy.get(".title").should("have.text", "Products").and("be.visible"),
 
   AssertLoginWithInvalidData: () =>
     cy.get(".error-message-container").should("be.visible"),
