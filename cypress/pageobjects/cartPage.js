afeb0f5777be06037dbde237051b8e7bea0b52c5
Dwiky Kurniawan <dwiky24kurniawan@gmail.com>
2024-03-21 15:02:58 +0700

    menambahkan element locator

--- cypress/pageobjects/cartPage.js
+++ cypress/pageobjects/cartPage.js
@@ -2,4 +2,7 @@ export let CartPage = {
     getCartListItems: () => cy.get(".cart_item").should("be.visible"),
 
     getButtonCheckOut: () => cy.get("#checkout").should("be.visible"),
+
+    AssertCartURL: () =>
+    cy.url().should("include", "/cart.html"),
 }
